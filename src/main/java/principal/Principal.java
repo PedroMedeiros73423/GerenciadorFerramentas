e467313dff9d7cd9ebe3b82761eaaafcaefea3a7
Turchiellows <root@DELL-7559>
2024-10-08 18:45:53 -0300

    mod: Finalizada a estrutura inicial do projeto com todos os alunos presente.

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -5,18 +5,25 @@ package principal;
 public class Principal {
 
    public static void main(String[] args) {
-      // Instanciar
+      // INSTANCIAR
       Elias aluno1 = new Elias();
+      Joaoiuncks aluno2 = new Joaoiuncks();
+      Pedro aluno3 = new Pedro();
       
       
+      // IDENTIFICAÇÃO DOS ALUNOS
       System.out.println("======================================================");
       System.out.println("PROJETO A3");
       System.out.println("======================================================");
       System.out.println("MEMBROS DO GRUPO");
       System.out.println("Aluno 01: " + aluno1.getNome());
-      // Adicionar os outros nomes aqui
-      
+      System.out.println("Aluno 02: " + aluno2.getNome());
+      System.out.println("Aluno 03: " + aluno3.getNome());
       System.out.println("");
+      
+      
+      
+      
       
    }
 }
