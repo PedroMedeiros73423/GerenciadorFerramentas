1e165fff9aa10a5e64d8cf97d9afeeeedeb27616
Turchiellows <[turchiellows@yahoo.com.br](mailto:turchiellows@yahoo.com.br)>
2024-10-31 18:51:58 -0300

    add: Teste de usuário 5

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -8,7 +8,6 @@ public class Principal {
 
    public static void main(String[] args) {
       // INSTANCIAR INTERFACE GRÁFICA
-      
       Index dashboard = new Index();
       dashboard.setVisible(true);
       dashboard.setLocationRelativeTo(null);
