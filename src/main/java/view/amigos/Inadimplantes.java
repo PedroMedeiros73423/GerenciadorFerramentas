aee3b0633b62d148a9480dc2b2f660e6f8ae2714
Pedro <Pedro@192.168.0.5>
2024-11-15 11:48:44 -0200

    feat: adição do interface Inadimplantes.java

---
+++ src/main/java/view/amigos/Inadimplantes.java
@@ -0,0 +1,160 @@
+
+package view.amigos;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import model.Amigos;
+
+
+public class Inadimplantes extends javax.swing.JFrame {
+// CRIANDO UM OBJETO DE MANIPULAÇÃO
+
+   Amigos manipulado = new Amigos();
+
+    public Inadimplantes() {
+        initComponents();
+        
+        // CARREGANDO A TABELA
+        this.carregaTabela();
+    }
+    // METODO PARA CARREGAR OS DADOS NA TABELA
+   public void carregaTabela() {
+      // LENDO O MODELO DA TABELA
+      DefaultTableModel modelo = (DefaultTableModel) this.tabelaAmigosInadimplentes.getModel();
+      modelo.setNumRows(0);
+
+      // BUSCANDO OS DADOS NO BANCO
+      ArrayList<Amigos> inadimplentes = manipulado.getInadimplentes();
+
+      if (inadimplentes.size() == 0) {
+         JOptionPane.showMessageDialog(null, "Boa notícia!!\n\nNão há amigos inadimplentes");
+      }
+
+      // INSRINDO OS DADOS NA TABELA
+      for (Amigos esteAmigo : inadimplentes) {
+         modelo.addRow(new Object[]{
+            esteAmigo.getAmigoNome(),
+            esteAmigo.getAmigoEmail(),
+            esteAmigo.getAmigoEndereco(),
+            esteAmigo.getAmigoTelefone(),
+            esteAmigo.getAmigoId()
+         });
+      }
+   }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        botaoVoltar = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabelaAmigosInadimplentes = new javax.swing.JTable();
+        botaoAtualizar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("AMIGOS INADIMPLANTES");
+
+        botaoVoltar.setText("Voltar");
+        botaoVoltar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoVoltarActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Os amigos listados abaixo contrairam empréstimos de ferramentas mas nunca as devolveram.");
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 48)); // NOI18N
+        jLabel1.setText("Amigos inadimplentes");
+
+        tabelaAmigosInadimplentes.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nome", "Email", "Endereco", "Telefone", "Qt ferramentas"
+            }
+        ));
+        jScrollPane1.setViewportView(tabelaAmigosInadimplentes);
+
+        botaoAtualizar.setText("Atualizar");
+        botaoAtualizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoAtualizarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                    .addGap(6, 6, 6)
+                                    .addComponent(jLabel1))
+                                .addGroup(layout.createSequentialGroup()
+                                    .addGap(325, 325, 325)
+                                    .addComponent(botaoAtualizar)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(botaoVoltar)
+                                    .addGap(34, 34, 34)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel2)))
+                        .addGap(0, 281, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 368, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(botaoAtualizar)
+                    .addComponent(botaoVoltar))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void botaoVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoVoltarActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_botaoVoltarActionPerformed
+
+    private void botaoAtualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoAtualizarActionPerformed
+        // recarregando a tabela:
+        this.carregaTabela();
+    }//GEN-LAST:event_botaoAtualizarActionPerformed
+
+
+    public static void main(String args[]) {
+      java.awt.EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            new Inadimplantes().setVisible(true);
+         }
+      });
+   }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botaoAtualizar;
+    private javax.swing.JButton botaoVoltar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tabelaAmigosInadimplentes;
+    // End of variables declaration//GEN-END:variables
+}
