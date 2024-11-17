05432fe110d9826cb376cf04481672379cbf3af5
Turchiellows <[turchiellows@yahoo.com.br](mailto:turchiellows@yahoo.com.br)>
2024-11-17 14:24:03 -0200

    fix: Eliminado erro nas telas de empréstimos

--- src/main/java/view/negocios/Atrasados.java
+++ src/main/java/view/negocios/Atrasados.java
@@ -1,4 +1,3 @@
-
 package view.negocios;
 
 import java.util.ArrayList;
@@ -7,16 +6,18 @@ import javax.swing.table.DefaultTableModel;
 import model.Negocios;
 
 public class Atrasados extends javax.swing.JFrame {
-    // CRIANDO OBJETOS DE MANIPULAÇÃO
+
+   // CRIANDO OBJETOS DE MANIPULAÇÃO
    Negocios manipulado = new Negocios();
-   
-    public Atrasados() {
-        initComponents();
+
+   public Atrasados() {
+      initComponents();
 
       // CARREGANDO A TABELA
       this.carregaTabela();
    }
-    // METODO PARA CARREGAR OS DADOS NA TABELA
+
+   // METODO PARA CARREGAR OS DADOS NA TABELA
    private void carregaTabela() {
       // LENDO O MODELO DA TABELA
       DefaultTableModel modelo = (DefaultTableModel) this.tablelaEmpresimosAtrasados.getModel();
@@ -41,28 +42,22 @@ public class Atrasados extends javax.swing.JFrame {
          });
       }
    }
-    @SuppressWarnings("unchecked")
+
+   @SuppressWarnings("unchecked")
    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
    private void initComponents() {
 
-      botaoVoltar = new javax.swing.JButton();
       jLabel1 = new javax.swing.JLabel();
       jScrollPane1 = new javax.swing.JScrollPane();
       tablelaEmpresimosAtrasados = new javax.swing.JTable();
       botaoAtualizar = new javax.swing.JButton();
+      botaoVoltar = new javax.swing.JButton();
 
       setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-      setTitle("EMPRÉSIMOS NÃO DEVOLVIDOS");
-
-      botaoVoltar.setText("Voltar");
-      botaoVoltar.addActionListener(new java.awt.event.ActionListener() {
-         public void actionPerformed(java.awt.event.ActionEvent evt) {
-            botaoVoltarActionPerformed(evt);
-         }
-      });
+      setTitle("Lista de empréstimos não devolvidos");
 
       jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
-      jLabel1.setText("Lista de Empréstimos não Devolvidos");
+      jLabel1.setText("Lista de empréstimos não devolvidos");
 
       tablelaEmpresimosAtrasados.setModel(new javax.swing.table.DefaultTableModel(
          new Object [][] {
@@ -73,11 +68,21 @@ public class Atrasados extends javax.swing.JFrame {
          }
       ));
       jScrollPane1.setViewportView(tablelaEmpresimosAtrasados);
+      if (tablelaEmpresimosAtrasados.getColumnModel().getColumnCount() > 0) {
+         tablelaEmpresimosAtrasados.getColumnModel().getColumn(0).setMaxWidth(30);
+      }
 
       botaoAtualizar.setText("Atualizar");
       botaoAtualizar.addActionListener(new java.awt.event.ActionListener() {
          public void actionPerformed(java.awt.event.ActionEvent evt) {
             botaoAtualizarActionPerformed(evt);
+         }
+      });
+
+      botaoVoltar.setText("Voltar");
+      botaoVoltar.addActionListener(new java.awt.event.ActionListener() {
+         public void actionPerformed(java.awt.event.ActionEvent evt) {
+            botaoVoltarActionPerformed(evt);
          }
       });
 
@@ -90,7 +95,7 @@ public class Atrasados extends javax.swing.JFrame {
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                .addGroup(layout.createSequentialGroup()
                   .addComponent(jLabel1)
-                  .addGap(0, 207, Short.MAX_VALUE))
+                  .addGap(0, 213, Short.MAX_VALUE))
                .addComponent(jScrollPane1))
             .addContainerGap())
          .addGroup(layout.createSequentialGroup()
@@ -102,62 +107,36 @@ public class Atrasados extends javax.swing.JFrame {
       );
       layout.setVerticalGroup(
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+         .addGroup(layout.createSequentialGroup()
             .addComponent(jLabel1)
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 465, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 454, Short.MAX_VALUE)
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                .addComponent(botaoAtualizar)
-               .addComponent(botaoVoltar)))
+               .addComponent(botaoVoltar))
+            .addContainerGap())
       );
 
       pack();
    }// </editor-fold>//GEN-END:initComponents
 
-    private void botaoVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoVoltarActionPerformed
-        this.dispose();
-    }//GEN-LAST:event_botaoVoltarActionPerformed
+   private void botaoVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoVoltarActionPerformed
+      this.dispose();
+   }//GEN-LAST:event_botaoVoltarActionPerformed
 
-    private void botaoAtualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoAtualizarActionPerformed
-        // RECARREGANDO A TABELA
-        this.carregaTabela();
-    }//GEN-LAST:event_botaoAtualizarActionPerformed
+   private void botaoAtualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoAtualizarActionPerformed
+      // RECARREGANDO A TABELA
+      this.carregaTabela();
+   }//GEN-LAST:event_botaoAtualizarActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Atrasados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Atrasados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Atrasados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Atrasados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Atrasados().setVisible(true);
-            }
-        });
-    }
+   public static void main(String args[]) {
+      java.awt.EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            new Atrasados().setVisible(true);
+         }
+      });
+   }
 
    // Variables declaration - do not modify//GEN-BEGIN:variables
    private javax.swing.JButton botaoAtualizar;
